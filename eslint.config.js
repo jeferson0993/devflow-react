772a36819cd2afc9eb8b8c08bc5d1afc83d60fe3
Jeferson f Silva <jeferson0993@gmail.com>
2024-11-04 20:33:28 -0300

    chore (maintain) - 🎨 - Improve structure / format of the code style (tailwindcss) - 💄 - Add or update the UI and style files
    Add eslint, prettier and tailwindcss

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,12 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { config as prettierConfig, rules as prettierRules } from 'eslint-plugin-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +24,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      ...prettierRules,
     },
   },
 )
